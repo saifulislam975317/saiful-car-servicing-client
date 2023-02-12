5584a75206a1dfaf3c75cdd3a38676fca831d8c4
saiful islam <saifulislam975317@gmail.com>
2023-02-12 23:18:26 +0600

    car-client-update-info

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ServicesCard from "./ServicesCard";
+
+//https://saiful-car-servicing-server.vercel.app/services
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const searchRef = useRef("");
+  const [isAsc, setIsAsc] = useState(true);
+  const [search, setSearch] = useState("");
   useEffect(() => {
-    fetch("https://saiful-car-servicing-server.vercel.app/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&order=${
+        isAsc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [isAsc, search]);
+
+  const handleSearch = () => {
+    const searchText = searchRef.current;
+    setSearch(searchText.value);
+    searchText.value = "";
+  };
   return (
     <div>
       <div className="text-center my-12">
@@ -19,6 +34,21 @@ const Services = () => {
           <br />
           or randomised words which don't look even slightly believable.
         </p>
+        <div className="text-end">
+          <span className="text-xl">Sort by:</span>
+          <button className="btn btn-warning " onClick={() => setIsAsc(!isAsc)}>
+            {isAsc ? "High Price to Low Price" : "Low Price to High Price"}
+          </button>
+        </div>
+        <input
+          type="text"
+          placeholder="Search "
+          ref={searchRef}
+          className="input input-bordered input-md w-full max-w-xs"
+        />
+        <button onClick={handleSearch} className="btn btn-md">
+          search
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
         {services.map((service) => (
